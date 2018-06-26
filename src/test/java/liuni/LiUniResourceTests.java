045031a8ab27a093b2e28dc638b1acf81ba1e5bc
dhagandev <dhagan@utexas.edu>
2018-06-26 14:57:59 -0500

    Refactored/Reformatted LiUniResourceTests

--- src/test/java/liuni/LiUniResourceTests.java
+++ src/test/java/liuni/LiUniResourceTests.java
@@ -2,6 +2,8 @@ package liuni;
 
 import liuni.api.ErrorModel;
 import liuni.resources.LiUniResource;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -14,6 +16,7 @@ import twitter4j.TwitterException;
 
 import javax.ws.rs.core.Response;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.reset;
@@ -30,250 +33,97 @@ public class LiUniResourceTests {
     @Before
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        twitterStatus.setTwitter(twitter);
+        resource.setTwitterStatus(twitterStatus);
+    }
+
+    @After
+    public void tearDown() {
+        reset(twitter);
+        reset(status);
     }
 
     /* Test LiUniResource .fetchTimeline() REST method */
     @Test
-    public void testREST_fetchTimeline_emptyTimeline_Status() {
-        resource = new LiUniResource();
-        twitterTimeline = new TwitterTimeline();
-        twitterTimeline.setTwitter(twitter);
-        resource.setTwitterTimeline(twitterTimeline);
-
+    public void testREST_fetchTimeline_emptyTimeline() {
         ResponseList<Status> respList = new ResponseListImpl<Status>();
-        boolean testResult;
         try {
             doReturn(respList).when(twitter).getHomeTimeline();
 
             Response resp = resource.fetchTimeline();
-            testResult = resp.getStatus() == Response.Status.OK.getStatusCode();
+            boolean statusCheck = resp.getStatus() == Response.Status.OK.getStatusCode();
+            boolean entityCheck = resp.hasEntity() && resp.getEntity().equals(respList);
+
+            assertEquals(statusCheck && entityCheck, true);
         }
         catch (Exception e) {
-            testResult = false;
+            Assert.fail();
         }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
     }
 
     @Test
-    public void testREST_fetchTimeline_emptyTimeline_Entity() {
-        resource = new LiUniResource();
-        twitterTimeline = new TwitterTimeline();
-        twitterTimeline.setTwitter(twitter);
-        resource.setTwitterTimeline(twitterTimeline);
-
+    public void testREST_fetchTimeline_updateTimeline() {
         ResponseList<Status> respList = new ResponseListImpl<Status>();
-        boolean testResult;
         try {
             doReturn(respList).when(twitter).getHomeTimeline();
 
             Response resp = resource.fetchTimeline();
-            testResult = resp.hasEntity() && resp.getEntity().equals(respList);
-        }
-        catch (Exception e) {
-            testResult = false;
-        }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
-    }
-
-    @Test
-    public void testREST_fetchTimeline_updateTimeline_Status() {
-        resource = new LiUniResource();
-        twitterTimeline = new TwitterTimeline();
-        twitterTimeline.setTwitter(twitter);
-        resource.setTwitterTimeline(twitterTimeline);
-
-        ResponseList<Status> respList = new ResponseListImpl<Status>();
-        boolean testResult;
-        try {
-            doReturn(respList).when(twitter).getHomeTimeline();
-
-            Response resp = resource.fetchTimeline();
-            testResult = resp.getStatus() == Response.Status.OK.getStatusCode();
-            assertTrue(testResult);
+            boolean statusCheck = resp.getStatus() == Response.Status.OK.getStatusCode();
+            boolean entityCheck  = resp.hasEntity() && resp.getEntity().equals(respList);
+            assertEquals(statusCheck && entityCheck, true);
 
             respList.add(status);
             resp = resource.fetchTimeline();
-            testResult = resp.getStatus() == Response.Status.OK.getStatusCode();
+            statusCheck = resp.getStatus() == Response.Status.OK.getStatusCode();
+            entityCheck = resp.hasEntity() && resp.getEntity().equals(respList);
+
+            assertEquals(statusCheck && entityCheck, true);
         }
         catch (Exception e) {
-            testResult = false;
+            Assert.fail();
         }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
     }
 
     @Test
-    public void testREST_fetchTimeline_updateTimeline_Entity() {
-        resource = new LiUniResource();
-        twitterTimeline = new TwitterTimeline();
-        twitterTimeline.setTwitter(twitter);
-        resource.setTwitterTimeline(twitterTimeline);
-
-        ResponseList<Status> respList = new ResponseListImpl<Status>();
-        boolean testResult;
-        try {
-            doReturn(respList).when(twitter).getHomeTimeline();
-
-            Response resp = resource.fetchTimeline();
-            testResult = resp.hasEntity() && resp.getEntity().equals(respList);
-            assertTrue(testResult);
-
-            respList.add(status);
-            resp = resource.fetchTimeline();
-            testResult = resp.hasEntity() && resp.getEntity().equals(respList);
-        }
-        catch (Exception e) {
-            testResult = false;
-        }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
-    }
-
-    @Test
-    public void testREST_fetchTimeline_fetchTimelineError_Status() {
-        resource = new LiUniResource();
-        twitterTimeline = new TwitterTimeline();
-        twitterTimeline.setTwitter(twitter);
-        resource.setTwitterTimeline(twitterTimeline);
-
+    public void testREST_fetchTimeline_fetchTimelineError() {
         ResponseList<Status> respList = new ResponseListImpl<Status>();
         respList.add(status);
-        boolean testResult;
-        try {
-            when(twitter.getHomeTimeline()).thenThrow(new TwitterException(""));
-
-            Response resp = resource.fetchTimeline();
-            testResult = resp.getStatus() == Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
-        }
-        catch (Exception e) {
-            testResult = false;
-        }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
-    }
-
-    @Test
-    public void testREST_fetchTimeline_fetchTimelineError_Entity() {
-        resource = new LiUniResource();
-        twitterTimeline = new TwitterTimeline();
-        twitterTimeline.setTwitter(twitter);
-        resource.setTwitterTimeline(twitterTimeline);
-
-        ResponseList<Status> respList = new ResponseListImpl<Status>();
-        respList.add(status);
-        boolean testResult;
         String expectedError = (new ErrorModel()).getGeneralError();
         try {
             when(twitter.getHomeTimeline()).thenThrow(new TwitterException(""));
 
             Response resp = resource.fetchTimeline();
-            testResult = resp.getEntity().toString().equals(expectedError);
+            boolean statusCheck = resp.getStatus() == Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+            boolean entityCheck = resp.getEntity().toString().equals(expectedError);
+
+            assertEquals(statusCheck && entityCheck, true);
         }
         catch (Exception e) {
-            testResult = false;
+            Assert.fail();
         }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
     }
 
     /* Test LiUniResource .postTweet() REST method */
     @Test
-    public void testREST_postTweet_successfulPost_Status() {
-        resource = new LiUniResource();
-        twitterStatus = new TwitterStatus();
-        twitterStatus.setTwitter(twitter);
-        resource.setTwitterStatus(twitterStatus);
-
-        boolean testResult;
+    public void testREST_postTweet_successfulPost() {
         String testString = "message";
         try {
             when(twitter.updateStatus(testString)).thenReturn(status);
             when(status.getText()).thenReturn(testString);
 
             Response resp = resource.postTweet(testString);
-            testResult = resp.getStatus() == Response.Status.CREATED.getStatusCode();
+            boolean statusCheck = resp.getStatus() == Response.Status.CREATED.getStatusCode();
+            boolean entityCheck = resp.getEntity().toString().equals(testString);
+
+            assertEquals(statusCheck && entityCheck, true);
         }
         catch (Exception e) {
-            testResult = false;
+            Assert.fail();
         }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
     }
 
     @Test
-    public void testREST_postTweet_successfulPost_Entity() {
-        resource = new LiUniResource();
-        twitterStatus = new TwitterStatus();
-        twitterStatus.setTwitter(twitter);
-        resource.setTwitterStatus(twitterStatus);
-
-        boolean testResult;
-        String testString = "message";
-        try {
-            when(twitter.updateStatus(testString)).thenReturn(status);
-            when(status.getText()).thenReturn(testString);
-
-            Response resp = resource.postTweet(testString);
-            testResult = resp.getEntity().toString().equals(testString);
-        }
-        catch (Exception e) {
-            testResult = false;
-        }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
-    }
-
-    @Test
-    public void testREST_postTweet_badPost_badTweetStatus() {
-        resource = new LiUniResource();
-        twitterStatus = new TwitterStatus();
-        twitterStatus.setTwitter(twitter);
-        resource.setTwitterStatus(twitterStatus);
-
-        boolean testResult;
-        String testString = "";
-        try {
-            when(twitter.updateStatus(testString)).thenReturn(status);
-            when(status.getText()).thenReturn(testString);
-
-            Response resp = resource.postTweet(testString);
-            testResult = resp.getStatus() == Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
-        }
-        catch (Exception e) {
-            testResult = false;
-        }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
-    }
-
-    @Test
-    public void testREST_postTweet_badPost_badTweetEntity() {
-        resource = new LiUniResource();
-        twitterStatus = new TwitterStatus();
-        twitterStatus.setTwitter(twitter);
-        resource.setTwitterStatus(twitterStatus);
-
-        boolean testResult;
+    public void testREST_postTweet_badPost_badTweet() {
         String testString = "";
         String expectedError = (new ErrorModel()).getBadTweetError();
         try {
@@ -281,64 +131,32 @@ public class LiUniResourceTests {
             when(status.getText()).thenReturn(testString);
 
             Response resp = resource.postTweet(testString);
-            testResult = resp.getEntity().toString().equals(expectedError);
+            boolean statusCheck = resp.getStatus() == Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+            boolean entityCheck = resp.getEntity().toString().equals(expectedError);
+
+            assertEquals(statusCheck && entityCheck, true);
         }
         catch (Exception e) {
-            testResult = false;
+            Assert.fail();
         }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
     }
 
     @Test
-    public void testREST_postTweet_badPost_generalErrorStatus() {
-        resource = new LiUniResource();
-        twitterStatus = new TwitterStatus();
-        twitterStatus.setTwitter(twitter);
-        resource.setTwitterStatus(twitterStatus);
-
-        boolean testResult;
-        String testString = "Bad Message; Exception testing.";
-        try {
-            when(twitter.updateStatus(testString)).thenThrow(new TwitterException(""));
-
-            Response resp = resource.postTweet(testString);
-            testResult = resp.getStatus() == Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
-        }
-        catch (Exception e) {
-            testResult = false;
-        }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
-    }
-
-    @Test
-    public void testREST_postTweet_badPost_generalErrorEntity() {
-        resource = new LiUniResource();
-        twitterStatus = new TwitterStatus();
-        twitterStatus.setTwitter(twitter);
-        resource.setTwitterStatus(twitterStatus);
-
-        boolean testResult;
+    public void testREST_postTweet_badPost_generalError() {
         String testString = "Bad Message; Exception testing.";
         String expectedError = (new ErrorModel()).getGeneralError();
         try {
             when(twitter.updateStatus(testString)).thenThrow(new TwitterException(""));
 
             Response resp = resource.postTweet(testString);
-            testResult = resp.getEntity().toString().equals(expectedError);
+            boolean statusCheck = resp.getStatus() == Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+            boolean entityCheck = resp.getEntity().toString().equals(expectedError);
+
+            assertEquals(statusCheck && entityCheck, true);
         }
         catch (Exception e) {
-            testResult = false;
+            Assert.fail();
         }
-
-        assertTrue(testResult);
-        reset(twitter);
-        reset(status);
     }
 
 }
