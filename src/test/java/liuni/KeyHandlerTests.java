f2d984e372a9862f2e51bd3aa432a70f5a4f9edd
dhagandev <dhagan@utexas.edu>
2018-06-26 15:20:36 -0500

    Removed unused imports

--- src/test/java/liuni/KeyHandlerTests.java
+++ src/test/java/liuni/KeyHandlerTests.java
@@ -10,8 +10,6 @@ import twitter4j.Twitter;
 import java.io.File;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class KeyHandlerTests {
 
